09b6733df94a90f99f3261cd9cca6f06c62c6d99
Axel <axelyleya2011@gmail.com>
2022-02-15 10:15:10 +0100

    completed allure configuration

--- src/test/java/runners/testRunner.java
+++ src/test/java/runners/testRunner.java
@@ -6,15 +6,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
         features = "src/test/resources/features/test.feature"
         ,glue= {"classpath:stepdef"},
-        //plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
         plugin = { "pretty", "summary"},
         monochrome = true
 )
 public class testRunner extends AbstractTestNGCucumberTests {
-/*    @AfterClass
-    public static void writeExtentReport() {
-        Reporter.loadXMLConfig(new File("config/report.xml"));
 
-    }
-*/
 }
